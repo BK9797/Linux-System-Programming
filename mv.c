069b622ff73b9fc43cf2cf123e67d0ab485d3d91
Bishoy Kamel <139723745+BK9797@users.noreply.github.com>
2025-03-01 10:30:39 +0000

    Add files via upload

---
+++ mv.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <source_file> <destination_file>\n", argv[0]);
+        return 1;
+    }
+
+    if (rename(argv[1], argv[2]) == -1) {
+        perror("rename");
+        return 1;
+    }
+
+    return 0;
+}
+
